f6032e5972ad478058b7443dc61342734aa3b135
Robert Landers <landers.robert@gmail.com>
2023-03-11 20:32:01 +0100

    figure out dynamic stuff better

--- dev.php
+++ dev.php
@@ -1,19 +1,32 @@
 <?php
 
+//passthru('loft use vcluster');
+//passthru('devspace use namespace once');
+
 echo "Gathering secrets and generating required parameters...\n";
 $secrets = json_decode(`kubectl get secret swytch-once -o json`, true);
 $secrets = array_map(fn($secret) => base64_decode($secret), $secrets['data']);
 
+echo "Secrets:\n";
+foreach ($secrets as $key => $secret) {
+	echo "  $key: $secret\n";
+}
+
 $currentCluster = explode('_', `kubectl config current-context`)[1];
 $currentCluster = explode('-', $currentCluster)[1];
 
-$tag = `git rev-parse HEAD`;
-$config = yaml_parse_file(__DIR__.'/.devspace/cache.yaml');
+$deployed = json_decode(`kubectl get pods -o json`, true);
+$tag = explode(':', $deployed['items'][0]['spec']['containers'][0]['image'], 2)[1];
+
+echo "Deployed tag: $tag\n";
+
+$cacheFile = __DIR__ . '/.devspace/cache.yaml';
+$config = file_exists($cacheFile) ? yaml_parse_file($cacheFile) : [];
 $config['images']['once']['tag'] = $tag;
-yaml_emit_file(__DIR__.'/.devspace/cache.yaml', $config);
+yaml_emit_file(__DIR__ . '/.devspace/cache.yaml', $config);
 
 passthru(
-	"devspace dev --force-deploy --skip-build -t  $tag" .
+	"devspace dev --force-deploy --skip-build -t  $tag " .
 	"--var STATE_SECRET='{$secrets['state-secret']}' " .
 	"--var RETHINKDB_PASSWORD='{$secrets['rdb-password']}' " .
 	"--var AUTH_LOGIN='https://fake-auth.{$currentCluster}.once.getswytch.com/login' " .
